806c0c3251424dcd3da7ef055a508191b8b6ae4d
Veolinan <104050582+Veolinan@users.noreply.github.com>
2023-10-29 20:56:47 +0300

    Added morsecode

--- 3P_ower_hadler/3P_ower_hadler.ino
+++ 3P_ower_hadler/3P_ower_hadler.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial sim(2, 3);
+SoftwareSerial sim(3, 2);
 const int analogPin = A0;
 const int ledPin = 12;
 const int threshold = 100; // Set the threshold value as per your requirement
@@ -8,17 +8,26 @@ const String number1 = "+254704358072";     // Your first number
 const String number2 = "+254769189571"; // Your second number
 const String number3 = "+254785303908"; // Your third number
 
+#define BUZZER_PIN 5  // Define the buzzer pin as digital pin 5
+
+// Morse code timing
+#define DOT_DURATION 250   // Duration of a dot in milliseconds
+#define DASH_DURATION 1000 // Duration of a dash in milliseconds
+#define SYMBOL_SPACE 250   // Gap between dots and dashes within a character
+#define LETTER_SPACE 500   // Gap between letters
+#define WORD_SPACE 1000    // Gap between words
+
 bool ledOn = false; // Track the LED state
 bool callInProgress = false; // Track if a call is in progress
-bool callRedirected = false; // Track if a call has been redirected
 unsigned long callStartTime = 0; // Track the start time of the call
-const long callDuration = 30000; // Call duration in milliseconds (20 seconds)
-const long callRedirectTime = 1500; // Time before redirecting the call in milliseconds (15 seconds)
+const long callDuration = 30000; // Call duration in milliseconds (30 seconds)
+const long callRedirectTime = 15000; // Time before redirecting the call in milliseconds (15 seconds)
 int currentNumber = 1; // 1 for the first number, 2 for the second number, 3 for the third number
 
 void setup() {
   pinMode(analogPin, INPUT);
   pinMode(ledPin, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT); // Set the buzzer pin as an output
   digitalWrite(ledPin, LOW); // Ensure the LED is initially off
   delay(7000); // Delay for 7 seconds to ensure the modules get the signal
   Serial.begin(9600);
@@ -34,9 +43,9 @@ void loop() {
   if (sensorValue < threshold && !callInProgress) {
     // A0 is low, initiate the call and start blinking the LED
     currentNumber = 1; // Always start from the first number when A0 is low
-    callNumber();
+    sendSMSAndCall();
+    playMorseCode("KIV"); // Play Morse code when sensorValue is below the threshold
     ledOn = true; // Turn on the LED
-    callRedirected = false; // Reset the redirected flag when a new call is initiated
   } else if (sensorValue >= threshold) {
     // A0 is high, terminate the call and stop blinking the LED
     if (callInProgress) {
@@ -52,11 +61,9 @@ void loop() {
       // Call duration has elapsed, hang up the call
       hangUpCall();
       cycleNumbers(); // Cycle to the next number
-      callRedirected = false; // Reset the redirected flag when a new cycle starts
-    } else if (!callRedirected && currentMillis - callStartTime >= callRedirectTime) {
-      // Redirect the call to the other number after callRedirectTime only once per cycle
+    } else if (currentMillis - callStartTime >= callRedirectTime) {
+      // Redirect the call to the other number after callRedirectTime
       redirectCall();
-      callRedirected = true; // Set the redirected flag after redirecting a call
     }
   }
 
@@ -73,7 +80,7 @@ void loop() {
   }
 }
 
-void callNumber() {
+void sendSMSAndCall() {
    String targetNumber;
    
    switch(currentNumber) {
@@ -90,6 +97,9 @@ void callNumber() {
        return; 
    }
    
+   // Send an SMS before making the call
+   sendSMS(targetNumber);
+
    sim.print("ATD");
    sim.print(targetNumber);
    sim.print(";\r\n");
@@ -97,6 +107,21 @@ void callNumber() {
 
    callInProgress = true;
    callStartTime = millis();
+}
+
+void sendSMS(String targetNumber) {
+  // Select the SMS storage
+  sim.println("AT+CMGF=1"); // Set SMS mode to text
+  delay(100);
+
+  // Compose the SMS and send it
+  sim.print("AT+CMGS=\"");
+  sim.print(targetNumber);
+  sim.println("\"");
+  delay(100);
+  sim.print("Power out"); // Your SMS message
+  sim.print((char)26); // Ctrl+Z to send SMS
+  delay(100);
 }
 
 void hangUpCall() {
@@ -107,29 +132,36 @@ void hangUpCall() {
 
 void redirectCall() {
    cycleNumbers(); 
-   if(currentNumber ==2){
-     sendSMS(number2,"Power out, send maintenance");
-   }
-   else if(currentNumber ==3){
-     sendSMS(number3,"Kilo India Victor Unserviceable is initiated");
-   }
-   callNumber(); 
+   sendSMSAndCall(); 
 }
 
 void cycleNumbers() {
    currentNumber++;
    
    if(currentNumber > 3) {
-     currentNumber =1;
-}
+     currentNumber=1;
+   }
 }
 
-void sendSMS(String number, String message){
-   sim.println("AT+CMGF=1"); 
-   delay(100);
-   sim.println("AT+CMGS=\""+number+"\"");
-   delay(100);
-   sim.println(message); 
-   delay(100);
-   sim.println((char)26); 
+void playMorseCode(String morseCode) {
+  for (int i = 0; i < morseCode.length(); i++) {
+    if (morseCode[i] == '.')
+      playDot();
+    else if (morseCode[i] == '-')
+      playDash();
+    delay(SYMBOL_SPACE); // Gap between dots and dashes within a character
+  }
+  delay(LETTER_SPACE - SYMBOL_SPACE); // Gap between letters
+}
+
+void playDot() {
+  tone(BUZZER_PIN, 2000, DOT_DURATION); // Increase frequency to make it louder
+  delay(DOT_DURATION);
+  noTone(BUZZER_PIN);
+}
+
+void playDash() {
+  tone(BUZZER_PIN, 2000, DASH_DURATION); // Increase frequency to make it louder
+  delay(DASH_DURATION);
+  noTone(BUZZER_PIN);
 }
