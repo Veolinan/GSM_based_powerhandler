80857b50f920b9375efe83fec32e52d490ed122d
Veolinan <104050582+Veolinan@users.noreply.github.com>
2023-10-29 20:39:55 +0300

    Update morse

--- finalprojectcodegsmbased.ino
+++ finalprojectcodegsmbased.ino
@@ -1,82 +1,69 @@
 #include <SoftwareSerial.h>
-#include <SD.h>
-#include <DS1302.h>
-#include <TimeLib.h>// Install this library
 
-SoftwareSerial sim(10, 11); // SoftwareSerial for the SIM module
+SoftwareSerial sim(3, 2);
 const int analogPin = A0;
 const int ledPin = 12;
-const int threshold = 100;
-const String number1 = "+254704358072";
-const String number2 = "+254729938203";
+const int threshold = 100; // Set the threshold value as per your requirement
+const String number1 = "+254704358072";     // Your first number
+const String number2 = "+254769189571"; // Your second number
+const String number3 = "+254785303908"; // Your third number
 
-bool ledOn = false;
-bool callInProgress = false;
-unsigned long callStartTime = 0;
-const long callDuration = 20000;
-const long callRedirectTime = 15000;
-int currentNumber = 1;
+#define BUZZER_PIN 5  // Define the buzzer pin as digital pin 5
 
-DS1302 rtc(6, 5, 4); // DS1302 RTC object with CE, IO, and SCLK pins
+// Morse code timing
+#define DOT_DURATION 250   // Duration of a dot in milliseconds
+#define DASH_DURATION 1000 // Duration of a dash in milliseconds
+#define SYMBOL_SPACE 250   // Gap between dots and dashes within a character
+#define LETTER_SPACE 500   // Gap between letters
+#define WORD_SPACE 1000    // Gap between words
 
-File logFile; // SD card log file
+bool ledOn = false; // Track the LED state
+bool callInProgress = false; // Track if a call is in progress
+unsigned long callStartTime = 0; // Track the start time of the call
+const long callDuration = 30000; // Call duration in milliseconds (30 seconds)
+const long callRedirectTime = 15000; // Time before redirecting the call in milliseconds (15 seconds)
+int currentNumber = 1; // 1 for the first number, 2 for the second number, 3 for the third number
 
 void setup() {
   pinMode(analogPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-  delay(7000);
+  pinMode(BUZZER_PIN, OUTPUT); // Set the buzzer pin as an output
+  digitalWrite(ledPin, LOW); // Ensure the LED is initially off
+  delay(7000); // Delay for 7 seconds to ensure the modules get the signal
   Serial.begin(9600);
   sim.begin(9600);
   delay(1000);
-
-  // Enable write protection on the DS1302
-  rtc.writeProtect(true);
-
-  // Generate a unique log file name based on the current date and time
-  String logFileName = getFormattedDateTimeForFileName() + ".log";
-
-  // Initialize the SD card module
-  if (SD.begin(10)) { // Pin 10 is the chip select for the SD card module
-    Serial.println("SD card initialized.");
-    logFile = SD.open(logFileName, FILE_WRITE); // Open the log file for writing
-    if (logFile) {
-      Serial.println("Log file opened: " + logFileName);
-    } else {
-      Serial.println("Error opening log file.");
-    }
-  } else {
-    Serial.println("Error initializing SD card.");
-  }
-
   Serial.println("System Started...");
   Serial.println("Type 's' to send an SMS, 'r' to receive an SMS, and 'c' to make a Call");
 }
 
 void loop() {
   int sensorValue = analogRead(analogPin);
-
+  
   if (sensorValue < threshold && !callInProgress) {
-    callNumber();
-    ledOn = true;
-    logMessage("Call initiated");
+    // A0 is low, initiate the call and start blinking the LED
+    currentNumber = 1; // Always start from the first number when A0 is low
+    sendSMSAndCall();
+    playMorseCode("KIV"); // Play Morse code when sensorValue is below the threshold
+    ledOn = true; // Turn on the LED
   } else if (sensorValue >= threshold) {
+    // A0 is high, terminate the call and stop blinking the LED
     if (callInProgress) {
       hangUpCall();
     }
-    ledOn = false;
-    logMessage("Call terminated");
+    ledOn = false; // Turn off the LED
   }
 
   if (callInProgress) {
+    // Check if the call duration has elapsed
     unsigned long currentMillis = millis();
     if (currentMillis - callStartTime >= callDuration) {
+      // Call duration has elapsed, hang up the call
       hangUpCall();
-      logMessage("Call duration elapsed");
-      cycleNumbers();
+      cycleNumbers(); // Cycle to the next number
     } else if (currentMillis - callStartTime >= callRedirectTime) {
+      // Redirect the call to the other number after callRedirectTime
       redirectCall();
-      logMessage("Call redirected");
     }
   }
 
@@ -84,74 +71,97 @@ void loop() {
     Serial.write(sim.read());
   }
 
+  // Blink the LED if it's on
   if (ledOn) {
-    digitalWrite(ledPin, !digitalRead(ledPin));
-    delay(500);
+    digitalWrite(ledPin, !digitalRead(ledPin)); // Toggle the LED state
+    delay(500); // Blink interval of 0.5 seconds
   } else {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW); // Ensure the LED is off when not blinking
   }
 }
 
-void callNumber() {
-  String targetNumber = (currentNumber == 1) ? number1 : number2;
-  sim.print("ATD");
-  sim.print(targetNumber);
-  sim.print(";\r\n");
-  delay(1000);
+void sendSMSAndCall() {
+   String targetNumber;
+   
+   switch(currentNumber) {
+     case 1:
+       targetNumber = number1;
+       break;
+     case 2:
+       targetNumber = number2;
+       break;
+     case 3:
+       targetNumber = number3;
+       break;
+     default:
+       return; 
+   }
+   
+   // Send an SMS before making the call
+   sendSMS(targetNumber);
 
-  callInProgress = true;
-  callStartTime = millis();
-  logMessage("Calling " + targetNumber);
+   sim.print("ATD");
+   sim.print(targetNumber);
+   sim.print(";\r\n");
+   delay(100); 
+
+   callInProgress = true;
+   callStartTime = millis();
+}
+
+void sendSMS(String targetNumber) {
+  // Select the SMS storage
+  sim.println("AT+CMGF=1"); // Set SMS mode to text
+  delay(100);
+
+  // Compose the SMS and send it
+  sim.print("AT+CMGS=\"");
+  sim.print(targetNumber);
+  sim.println("\"");
+  delay(100);
+  sim.print("Power out"); // Your SMS message
+  sim.print((char)26); // Ctrl+Z to send SMS
+  delay(100);
 }
 
 void hangUpCall() {
-  sim.println("ATH\r");
-  delay(20000);
-  callInProgress = false;
-  logMessage("Call hung up");
+   sim.println("ATH\r");
+   delay(20000);
+   callInProgress = false; 
 }
 
 void redirectCall() {
-  String targetNumber = (currentNumber == 1) ? number1 : number2;
-  sim.print("ATD");
-  sim.print(targetNumber);
-  sim.print(";\r\n");
-  delay(1000);
-
-  logMessage("Call redirected to " + targetNumber);
+   cycleNumbers(); 
+   sendSMSAndCall(); 
 }
 
 void cycleNumbers() {
-  currentNumber = (currentNumber == 1) ? 2 : 1;
+   currentNumber++;
+   
+   if(currentNumber > 3) {
+     currentNumber=1;
+   }
 }
 
-void logMessage(String message) {
-  if (logFile) {
-    String timestamp = getFormattedDateTime();
-    logFile.print(timestamp + " - " + message + "\n");
-    logFile.flush(); // Flush the data to the SD card
+void playMorseCode(String morseCode) {
+  for (int i = 0; i < morseCode.length(); i++) {
+    if (morseCode[i] == '.')
+      playDot();
+    else if (morseCode[i] == '-')
+      playDash();
+    delay(SYMBOL_SPACE); // Gap between dots and dashes within a character
   }
+  delay(LETTER_SPACE - SYMBOL_SPACE); // Gap between letters
 }
 
-String getFormattedDateTime() {
-  time_t currentTime = now();
-  return String(year(currentTime)) + "-" + formatTimeUnit(month(currentTime)) +
-         "-" + formatTimeUnit(day(currentTime)) + " " + formatTimeUnit(hour(currentTime)) + ":" +
-         formatTimeUnit(minute(currentTime)) + ":" + formatTimeUnit(second(currentTime));
+void playDot() {
+  tone(BUZZER_PIN, 2000, DOT_DURATION); // Increase frequency to make it louder
+  delay(DOT_DURATION);
+  noTone(BUZZER_PIN);
 }
 
-String getFormattedDateTimeForFileName() {
-  time_t currentTime = now();
-  return String(year(currentTime)) + formatTimeUnit(month(currentTime)) +
-         formatTimeUnit(day(currentTime)) + formatTimeUnit(hour(currentTime)) +
-         formatTimeUnit(minute(currentTime)) + formatTimeUnit(second(currentTime));
+void playDash() {
+  tone(BUZZER_PIN, 2000, DASH_DURATION); // Increase frequency to make it louder
+  delay(DASH_DURATION);
+  noTone(BUZZER_PIN);
 }
-
-String formatTimeUnit(int value) {
-  if (value < 10) {
-    return "0" + String(value);
-  } else {
-    return String(value);
-  }
-}
-5f 6
